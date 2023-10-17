483fcc2d00fc719193a4213c5da94f147bf45e8e
rvahilario <rvahilario@gmail.com>
2023-10-16 21:35:02 -0300

    add: create initial theme style to start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme, darkTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <h1>Hello World!</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,43 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  background: '#FAFAFA',
+
+  'base-title': '#272221',
+  'base-subtitle': '#403937',
+  'base-text': '#574F4D',
+  'base-label': '#8D8686',
+  'base-hover': '#D7D5D5',
+  'base-button': '#E6E5E5',
+  'base-input': '#EDEDED',
+  'base-card': '#F3F2F2',
+
+  'purple-dark': '#4B2995',
+  purple: '#8047F8',
+  'purple-light': '#EBE5F9',
+
+  'yellow-dark': '#C47F17',
+  yellow: '#DBAC2C',
+  'yellow-light': '#F1E9C9',
+}
+
+export const darkTheme = {
+  white: '#000000', // Branco se torna preto
+  background: '#111111', // Fundo mais escuro
+
+  'base-title': '#FFFFFF', // Títulos mais claros
+  'base-subtitle': '#C0B6B5', // Subtítulos ligeiramente mais claros
+  'base-text': '#A9A2A1', // Texto mais claro
+  'base-label': '#6E6666', // Rótulos mais claros
+  'base-hover': '#393737', // Hover mais escuro
+  'base-button': '#333232', // Botões mais escuros
+  'base-input': '#2A2929', // Inputs mais escuros
+  'base-card': '#1F1E1E', // Cards mais escuros
+
+  'purple-dark': '#7643C7', // Roxo escuro
+  purple: '#5E36A3', // Roxo mais escuro
+  'purple-light': '#44317E', // Roxo mais claro
+
+  'yellow-dark': '#C7A42D', // Amarelo escuro
+  yellow: '#B58F26', // Amarelo mais escuro
+  'yellow-light': '#8D751E', // Amarelo mais claro
+}
