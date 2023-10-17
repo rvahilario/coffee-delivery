810ad299bf8ece803504d8279ef92cf9e3424041
rvahilario <rvahilario@gmail.com>
2023-10-17 00:47:47 -0300

    add: create DefaultLayout comp and router scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme, darkTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <h1>Hello World!</h1>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import { Outlet } from 'react-router-dom'
+import { Header } from '../components/Header'
+
+export function DefaultLayout() {
+  return (
+    <Container>
+      <Header />
+      <Outlet />
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 90rem;
+  padding: 0 10rem;
+  margin: 0 auto;
+`
