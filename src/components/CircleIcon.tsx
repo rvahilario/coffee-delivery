bf296e2c3223f9a9ee486965fbe2fb105b10448e
rvahilario <rvahilario@gmail.com>
2023-10-17 02:08:08 -0300

    add: create circle icon comp

---
+++ src/components/CircleIcon.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+import styled from 'styled-components'
+
+type CircleIconProps = {
+  icon: ReactNode
+  backgroundColor: string
+}
+
+export function CircleIcon({ icon, backgroundColor }: CircleIconProps) {
+  return <Container backgroundColor={backgroundColor}>{icon}</Container>
+}
+
+const Container = styled.div<{ backgroundColor: string }>`
+  background: ${({ backgroundColor }) => backgroundColor};
+  color: ${({ theme }) => theme.background};
+  height: 2rem;
+  width: 2rem;
+  border-radius: 50%;
+  padding: 0.5rem;
+`
