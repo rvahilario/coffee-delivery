b9afb37f3c171a01bc4d79efc097488d2d3a91d6
rvahilario <rvahilario@gmail.com>
2023-11-02 17:30:02 -0300

    fix: calculate total func according new checkout type

--- src/utils.ts
+++ src/utils.ts
@@ -5,18 +5,19 @@ export function formatCurrencyValue(value: number): string {
   return numeral(value).format('0,0.00')
 }
 
-export function calculateTotal(checkoutData: CheckoutData): {
+export function calculateTotal(selectedCoffees: SelectedCoffeesType): {
   totalItems: number
   deliveryTax: number
   total: number
 } {
-  const totalItems = checkoutData.selectedCoffees.reduce(
-    (total, coffee) =>
-      total + COFFEE_OBJECT[coffee.coffeeKey].price * coffee.quantity,
-    0,
-  )
+  const totalItems = Object.keys(selectedCoffees).reduce((total, coffeeKey) => {
+    const quantity = selectedCoffees[coffeeKey]
+    const coffeePrice = COFFEE_OBJECT[coffeeKey].price
+    return total + coffeePrice * quantity
+  }, 0)
 
   const deliveryTax = 5 // Fixed value or calculated based on distance
   const total = totalItems + deliveryTax
+
   return { totalItems, deliveryTax, total }
 }
